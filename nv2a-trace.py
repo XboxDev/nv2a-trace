706c11977f16cfefc5eb38c29dd4ff6f46795293
Jannik Vogel <email@jannikvogel.de>
2019-06-09 17:51:23 +0200

    Experiments with new tracing code

--- nv2a-trace.py
+++ nv2a-trace.py
@@ -2,6 +2,8 @@
 
 from xboxpy import *
 
+from helper import *
+
 # Create output folder
 import os
 try:
@@ -18,6 +20,7 @@ import traceback
 from helper import *
 
 import Trace
+
 
 abortNow = False
 
@@ -38,8 +41,11 @@ signal.signal(signal.SIGINT, signal_handler)
 class Xbox:
   def __init__(self):
     self.read_u32 = read_u32
-    self.read = read
     self.write_u32 = write_u32
+    self.read = read
+    self.write = write
+    self.call = api.call
+    self.ke = ke
 xbox = Xbox()
 
 xbox_helper = XboxHelper(xbox)
@@ -48,14 +54,13 @@ def main():
 
   global abortNow
 
-  DebugPrint = False
-
   print("\n\nSearching stable PB state\n\n")
   
   while True:
 
     # Stop consuming CACHE entries.
     xbox_helper.disable_pgraph_fifo()
+    xbox_helper.wait_until_pgraph_idle()
 
     # Kick the pusher, so that it fills the cache CACHE.
     xbox_helper.resume_fifo_pusher()
@@ -87,6 +92,8 @@ def main():
     # So we pause the pusher again to validate our state
     xbox_helper.pause_fifo_pusher()
 
+    time.sleep(1.0)
+
     v_dma_put_addr_target_check = xbox.read_u32(dma_put_addr)
     v_dma_get_addr_check = xbox.read_u32(dma_get_addr)
 
@@ -107,102 +114,50 @@ def main():
   # Start measuring time
   begin_time = time.monotonic()
 
-  # Step through the PB until we finish.
-  while(v_dma_get_addr != v_dma_put_addr_real):
+  bytes_queued = 0
 
-    print("@0x%08X; wants to be at 0x%08X" % (v_dma_get_addr, v_dma_put_addr_target))
+  # Create a new trace object
+  trace = Trace.Tracer(v_dma_get_addr, v_dma_put_addr_real)
 
-    # Get size of current command.
-    word = xbox.read_u32(0x80000000 | v_dma_get_addr)
-    v_dma_put_addr_target = parseCommand(v_dma_get_addr, word, DebugPrint)
+  # Step through the PB until we abort
+  while not abortNow:
 
-    # If we don't know where this command ends, we have to abort.
-    if v_dma_put_addr_target == 0:
-      print("Aborting due to unknown PB command")
-
-      # Recover the real address as Xbox would get stuck otherwise.
-      xbox.write_u32(dma_put_addr, v_dma_put_addr_real)
-
-      break
-
-    # Do whatever we want to do with the queued command / methods
     try:
 
-      # Emulate the current instruction stream.
-      #FIXME: This will create dumps, but nothing has actually run yet.
-      #       If one of the methods sets a register, and then another one draws
-      #       Then we won't have a state for that draw call.
-      #       We need better integration of this.
-      Trace.recordPushBufferCommand(xbox, v_dma_get_addr)
+      v_dma_get_addr, unprocessed_bytes = trace.processPushBufferCommand(xbox, xbox_helper, v_dma_get_addr)
+      bytes_queued += unprocessed_bytes
 
-    # Catches any errors so we don't leave the GPU in a bad state.
-    # We'll force quit now though
-    except Exception as err:
+      #time.sleep(0.5)
+
+      # Avoid queuing up too many bytes
+      if v_dma_get_addr == trace.real_dma_put_addr or bytes_queued >= 200:
+        print("Flushing buffer until (0x%08X)" % v_dma_get_addr)
+        trace.run_fifo(xbox, xbox_helper, v_dma_get_addr)
+        bytes_queued = 0
+        xbox_helper.dumpPBState()
+        X = 4
+        print(["PRE "] + ["%08X" % x for x in struct.unpack("<" + "L" * X, xbox.read(0x80000000 | (v_dma_get_addr - X * 4), X * 4))])
+        print(["POST"] + ["%08X" % x for x in struct.unpack("<" + "L" * X, xbox.read(0x80000000 | (v_dma_get_addr        ), X * 4))])
+
+      if v_dma_get_addr == trace.real_dma_put_addr:
+        break
+
+      # Verify we are where we think we are
+      if bytes_queued == 0:
+        v_dma_get_addr_real = xbox.read_u32(dma_get_addr)
+        print("Verifying hw (0x%08X) is at parser (0x%08X)" % (v_dma_get_addr_real, v_dma_get_addr))
+        try:
+          assert(v_dma_get_addr_real == v_dma_get_addr)
+        except:
+          xbox_helper.dumpPBState()
+          raise
+
+    except:
       traceback.print_exc()
       abortNow = True
 
-    # Queue this command
-    xbox.write_u32(dma_put_addr, v_dma_put_addr_target)
-
-    def JumpCheck(v_dma_put_addr_real):
-      # See if the PB target was modified.
-      # If necessary, we recover the current target to keep the GPU stuck on our
-      # current command.
-      v_dma_put_addr_new_real = xbox.read_u32(dma_put_addr)
-      if (v_dma_put_addr_new_real != v_dma_put_addr_target):
-        print("PB was modified! Got 0x%08X, but expected: 0x%08X; Restoring." % (v_dma_put_addr_new_real, v_dma_put_addr_target))
-        #FIXME: Ensure that the pusher is still disabled, or we might be
-        #       screwed already. Because the pusher probably pushed new data
-        #       to the CACHE which we attempt to avoid.
-
-        s1 = xbox.read_u32(put_state)
-        if s1 & 1:
-          print("PB was modified and pusher was already active!")
-          time.sleep(60.0)
-
-        xbox.write_u32(dma_put_addr, v_dma_put_addr_target)
-        v_dma_put_addr_real = v_dma_put_addr_new_real
-      return v_dma_put_addr_real
-
-    # Loop while this command is being ran.
-    # This is necessary because a whole command might not fit into CACHE.
-    # So we have to process it chunk by chunk.
-    command_base = v_dma_get_addr
-    while v_dma_get_addr >= command_base and v_dma_get_addr < v_dma_put_addr_target:
-      if DebugPrint: print("At 0x%08X, target is 0x%08X (Real: 0x%08X)" % (v_dma_get_addr, v_dma_put_addr_target, v_dma_put_addr_real))
-      if DebugPrint: xbox_helper.printDMAstate()
-
-      # Disable PGRAPH, so it can't run anything from CACHE.
-      xbox_helper.disable_pgraph_fifo()
-      xbox_helper.wait_until_pgraph_idle()
-
-      # This scope should be atomic.
-      if True:
-
-        # Avoid running bad code, if the PUT was modified sometime during
-        # this command.
-        v_dma_put_addr_real = JumpCheck(v_dma_put_addr_real)
-
-        # Kick our planned commands into CACHE now.
-        xbox_helper.resume_fifo_pusher()
-        xbox_helper.pause_fifo_pusher()
-
-      # Run the commands we have moved to CACHE, by enabling PGRAPH.
-      xbox_helper.enable_pgraph_fifo()
-
-      # Get the updated PB address.
-      v_dma_get_addr = xbox.read_u32(dma_get_addr)
-
-    # It's possible that the CPU updated the PUT after execution
-    v_dma_put_addr_real = JumpCheck(v_dma_put_addr_real)
-
-    # Also show that we processed the commands.
-    if DebugPrint: xbox_helper.dumpPBState()
-
-    # Check if the user wants to exit
-    if abortNow:
-      xbox.write_u32(dma_put_addr, v_dma_put_addr_real)
-      break
+  # Recover the real address
+  xbox.write_u32(dma_put_addr, trace.real_dma_put_addr)
 
   print("\n\nFinished PB\n\n")
 
@@ -213,8 +168,8 @@ def main():
   end_time = time.monotonic()
   duration = end_time - begin_time
 
-  flipStallCount = Trace.recordedFlipStallCount()
-  commandCount = Trace.recordedPushBufferCommandCount()
+  flipStallCount = trace.recordedFlipStallCount()
+  commandCount = trace.recordedPushBufferCommandCount()
   
   print("Recorded %d flip stalls and %d PB commands (%.2f commands / second)" % (flipStallCount, commandCount, commandCount / duration))
 
